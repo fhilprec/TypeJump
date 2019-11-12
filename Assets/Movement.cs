33a7daf107680dc040c45d97a1101ef7894c577f
fhilprec <44507214+fhilprec@users.noreply.github.com>
2019-11-13 00:00:48 +0100

    a

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,37 +1,96 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
 
 public class Movement : MonoBehaviour
 {
-    public int speed = 10;
-    public Rigidbody rb;
-    private string word = "";
-    public GameObject text;
-
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody cuberb;
+    public float speed = 1;
+    public float power = 1;
+    public string word = "";
+    private GameObject InputFieldg;
+    private bool clear = false;
+    private bool newstart = false;
+    private float deltimer = 0f;
+    private void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody>();
+        cuberb = gameObject.GetComponent<Rigidbody>();
+        cuberb.freezeRotation = true;
+    }
+    private void Update()
+    {
+        deltimer += Time.deltaTime;
+        cuberb.transform.position += Vector3.forward * speed * 1f * Time.deltaTime;
+        cuberb.AddForce(0, -power * 10f, 0);
+
+        if(clear && deltimer > 1f)
+        {
+            word = "";
+            clear = false;
+            InputFieldg.GetComponent<InputField>().text = "";
+        }
+        if(deltimer > 3f &&  InputFieldg != null)
+        {
+            word = "";
+            InputFieldg.GetComponent<InputField>().text = "";
+            deltimer = 0;
+        }
+        
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
 
-        Debug.Log(word);
-        
-            if (word == "right")
+
+
+    public void Changeposition(GameObject InputField)
+    {
+        if (newstart)
         {
-            //rb.AddForce(-speed * Time.deltaTime * 1000, 0, 0) ;
-            rb.AddForce(0, speed * Time.deltaTime * 1000, 0);
-            Debug.Log("Succes");
-            word = "";
+            InputFieldg.GetComponent<InputField>().text = "";
+            newstart = false;
         }
+
+        InputFieldg = InputField;
+        InputField.GetComponent<Animator>().Play(0);
+
+        word = InputField.GetComponent<InputField>().text;
+        Debug.Log(word);
         if(word.Length > 5)
         {
-            text.GetComponent<TextAsset>().text.Insert(0, "");
+            deltimer = 0;
+            clear = true; 
+            word = "";
+            newstart = true;
         }
-        if (Input.GetKey("o")) { word = ""; }
+        if(word == "right")
+        {
+            cuberb.transform.position += new Vector3(1, 0, 0);
+            deltimer = 0;
+            clear = true;
+            word = "";
+            newstart = true;
+        }
+        if (word == "left")
+        {
+            cuberb.transform.position += new Vector3(-1, 0, 0);
+            deltimer = 0;
+            clear = true;
+            word = "";
+            newstart = true;
+        }
+        if (word == "up")
+        {
+            cuberb.AddForce(0, power * 200f, 0);
+            deltimer = 0;
+            clear = true;
+            word = "";
+            newstart = true;
+        }
     }
+
+
+
+
 }
