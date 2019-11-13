238e9d25cc005d46b558912715f7eb6df15f1b13
fhilprec <44507214+fhilprec@users.noreply.github.com>
2019-11-13 13:27:03 +0100

    a

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,39 +8,112 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody cuberb;
     [Range(0.5f,5f)]
     public float speed;
-    private string word;
+    public float jumptime = 3;
+    public float power;
+    private string word = "";
+    public GameObject FadeOut;
+    public GameObject tempInput;
+    private bool clear = false;
+    private float time;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         cuberb = gameObject.GetComponent<Rigidbody>();
+        cuberb.transform.position += new Vector3(0, 0, 30);
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        jumptime += Time.deltaTime;
+        time += Time.deltaTime;
+        tempInput.GetComponent<InputField>().Select();
+
+
         cuberb.transform.position += new Vector3(0, 0, 10f * speed) * Time.deltaTime;
+
+        if(time > 0.75f)
+        {
+            clear = true;
+            ChangePosition(tempInput);
+
+        }
+        
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Obstacle")
+        {
+            Debug.Log("Bam");
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+            speed = 0;
+
+            //Hier Neustart Screen starten
+
+        }
+    }
 
     public void ChangePosition(GameObject InputField)
-    {
-        word = InputField.GetComponent<InputField>().text;
-        Debug.Log(word);
-
-        if(word == "right")
+    { 
+        time = 0;
+        if (clear)
         {
+            clear = false;
+            if(word != "") { Debug.Log(word[word.Length - 1]); }
+            
+            word = "";
+            InputField.GetComponent<InputField>().text = word;
+            FadeOut.GetComponent<InputField>().text = word;
+        }
+
+
+        InputField.GetComponent<Animator>().Play(0);
+
+        word = InputField.GetComponent<InputField>().text;
+
+        if(word == "right" && gameObject.transform.position.x <= 1.9f)
+        {
+            FadeOut.GetComponent<InputField>().text = word;
+            FadeOut.GetComponent<Animator>().Play(0);
+
             word = "";
             InputField.GetComponent<InputField>().text = "";
             cuberb.transform.position += new Vector3(1, 0, 0);
+            
         }
-        if (word == "left")
+        else if (word == "left" && gameObject.transform.position.x >= -1.9f)
         {
+            FadeOut.GetComponent<InputField>().text = word;
+            FadeOut.GetComponent<Animator>().Play(0);
+
             word = "";
             InputField.GetComponent<InputField>().text = "";
             cuberb.transform.position += new Vector3(-1, 0, 0);
         }
+        else if (word == "jump" && jumptime > 3f )
+        {
+            FadeOut.GetComponent<InputField>().text = word;
+            FadeOut.GetComponent<Animator>().Play(0);
+
+            word = "";
+            InputField.GetComponent<InputField>().text = "";
+            cuberb.AddForce(0, 10000f * Time.deltaTime * power, 0) ;
+            jumptime = 0;
+        }
+        else if(word.Length >= 5 || word == "left" && gameObject.transform.position.x < -1.9f)
+        {
+            FadeOut.GetComponent<InputField>().text = word;
+            FadeOut.GetComponent<Animator>().Play(0);
+
+            word = "";
+            InputField.GetComponent<InputField>().text = "";
+        }
+
 
     }
 
---
+++ Assets/Scripts/Spawn_Obstacles.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Obstacles : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Obstacle_small;
+    public GameObject Obstacle_big;
+    public int interval = 30;
+    public int spawnrange = 2;
+    [Range(0f,1f)]
+    public float chance = 0.5f;
+    [Range(0f, 1f)]
+    public float bigchance = 0.5f;
+    private Vector3 lastposition = new Vector3(0,0,0);
+
+    private void Start()
+    {
+        lastposition = Player.transform.position;
+
+        for (int i = 0; i < spawnrange - 1; i++)
+        {
+            float tempbigchance = bigchance;
+            for (int j = 0; j < 5; j++)
+            {
+
+                if (Random.Range(0f, 1f) < chance)
+                {
+                    if (Random.Range(0f, 1f) < tempbigchance)
+                    {
+                        GameObject obs = GameObject.Instantiate(Obstacle_big) as GameObject;
+                        obs.transform.position = new Vector3(0, 0.5f, interval + i * interval) + new Vector3(0, 0, (int)Player.transform.position.z) + new Vector3(j - 2, 0, 0 +interval);
+                        tempbigchance -= 0.25f;
+                    }
+                    else
+                    {
+                        GameObject obs = GameObject.Instantiate(Obstacle_small) as GameObject;
+                        obs.transform.position = new Vector3(0, 0, interval * 2 + i * interval) + new Vector3(0, 0, (int)Player.transform.position.z) + new Vector3(j - 2, 0, 0 + interval);
+                    }
+                }
+            }
+
+
+        }
+        lastposition = Player.transform.position;
+    }
+
+        // Update is called once per frame
+        
+        void FixedUpdate()
+        {
+            if (Player.transform.position.magnitude - lastposition.magnitude > interval)
+            {
+                lastposition = Player.transform.position;
+                float tempbigchance = bigchance;
+                for (int j = 0; j < 5; j++)
+                {
+
+                    if (Random.Range(0f, 1f) < chance)
+                    {
+                        if (Random.Range(0f, 1f) < tempbigchance)
+                        {
+                            GameObject obs = GameObject.Instantiate(Obstacle_big) as GameObject;
+                            obs.transform.position = new Vector3(0, 0.5f, interval + (spawnrange-1) * interval) + new Vector3(0, 0, (int)Player.transform.position.z) + new Vector3(j - 2, 0, 0);
+                            tempbigchance -= 0.25f;
+                        }
+                        else
+                        {
+                            GameObject obs = GameObject.Instantiate(Obstacle_small) as GameObject;
+                            obs.transform.position = new Vector3(0, 0, interval + (spawnrange-1) * interval) + new Vector3(0, 0, (int)Player.transform.position.z) + new Vector3(j - 2, 0, 0);
+                        }
+                    }
+
+                }
+            }
+        }
+        
+    }
+            
+            
+
+
+        
+ 
+
+   
+
