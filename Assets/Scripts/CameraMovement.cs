5aca190c1601a232f48ce25c4e4f7bad488f0211
Jan-01 <Jan-01@users.noreply.github.com>
2019-11-15 15:12:27 +0100

    a
    a

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(0, 2.5f, target.position.z-10);
+    }
+}
